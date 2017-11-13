7f18c1f2633fedf7a73d7e1d4be38184397b7b0c
yesheng <yexf_fyf@163.com>
2017-11-13 20:31:25 +0800

    @modify libwfc file

---
+++ libwfc/wfc_client_browser.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#include <include/cef_browser.h>
+#include "wfc_client_browser.h"
+
+wfcClientBrowser::wfcClientBrowser(HWND hWnd, LPCTSTR lpszMainPage)
+	:m_hWnd(hWnd), m_strMainPage(L"about:blank")
+{
+	OutputDebugString(L"wfcClientBrowser");
+	m_pHandle = new CefClientHandler();
+	if (lpszMainPage && lpszMainPage[0] != _T('\0'))
+	{
+		m_strMainPage = lpszMainPage;
+	}
+}
+
+wfcClientBrowser::~wfcClientBrowser()
+{
+	OutputDebugString(L"~wfcClientBrowser");
+	Stop();
+	m_pHandle = NULL;
+}
+
+
+wfcClientBrowser *wfcClientBrowser::CreateClientBrowser(HWND hWnd, LPCTSTR lpszMainPage)
+{
+	wfcClientBrowser *pBrowser = new wfcClientBrowser(hWnd, lpszMainPage);
+	RECT rect;
+	CefWindowInfo info;
+	CefBrowserSettings settings;
+	
+	GetClientRect(hWnd, &rect);
+	info.SetAsChild(hWnd, &rect);
+	CefBrowserHost::CreateBrowser(info, pBrowser->m_pHandle.get(), m_strMainPage, settings, NULL);
+	return pBrowser;
+}
+
+void wfcClientBrowser::Stop()
+{
+	if (m_pHandle.get())
+	{
+		if (!m_pHandle->IsClosing())
+		{
+			CefRefPtr<CefBrowser> browser = m_pHandle->GetBrowser();
+			if (browser.get())
+			{
+				browser.StopLoad();
+			}
+		}
+	}
+}
---
+++ libwfc/wfc_client_browser.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#ifndef WFC_CLIENT_BROWSER_H_
+#define WFC_CLIENT_BROWSER_H_
+#pragma once
+
+#include <set>
+#include "cef_client_handler.h"
+
+class wfcClientBrowser
+{
+public:
+	static wfcClientBrowser *CreateClientBrowser(HWND hWnd, LPCTSTR lpszMainPage);
+	~wfcClientBrowser();
+	
+	void Stop();
+	
+	bool hasClient(CefRefPtr<CefClient> pClient);
+private:
+	wfcClientBrowser();
+protected:
+	CefRefPtr<CefClientHandler> m_pHandle;
+	CefString m_strMainPage;
+	HWND m_hWnd;
+};
+
+#endif  // WFC_CLIENT_BROWSER_H_
