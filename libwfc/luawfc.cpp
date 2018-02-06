1014d85600846c058fa7dede3b04c20be2de9708
yesheng <yexf_fyf@163.com>
2018-02-06 16:24:33 +0800

    @modify add function get_version

--- libwfc/luawfc.cpp
+++ libwfc/luawfc.cpp
@@ -1,9 +1,27 @@
+#include <include/cef_version.h>
 #include "luawfc.h"
 #include "libwfc.h"
 #include "cef_client_scheme.h"
+
+static int lua_get_version(lua_State* L)
+{
+	char arChromeVer[256] = {0};
+	sprintf(arChromeVer, "%d.%d.%d.%d", CHROME_VERSION_MAJOR, CHROME_VERSION_MINOR,\
+		CHROME_VERSION_BUILD, CHROME_VERSION_PATCH);
+	lua_pushstring(L, arChromeVer);
+	lua_pushstring(L, CEF_VERSION);
+	lua_pushstring(L, CEF_COMMIT_HASH);
+	return 3;
+}
+
+const luaL_Reg l_lib[] = {
+	{"get_version", lua_get_version},
+	{0, 0}
+};
 
 int luaopen_libwfc(lua_State* L)
 {
 	CoInitializeCef();
 	RegisterSchemeHandlers();
+	luaL_register(L, "libwfc", l_lib);
 }
--- libwfc/luawfc.h
+++ libwfc/luawfc.h
@@ -3,7 +3,15 @@
 #pragma once
 #include "lua.hpp"
 #include "libwfc.h"
+
+
 #define PAI(L, n) (long) (lua_isnumber(L,n) ? luaL_checknumber((L), (n)) : 0)
+
+#define PAS(L, n) (lua_isstring(L,n) ? luaL_checkstring((L), (n)) : "")
+
+#define PASL(L, n) (lua_isstring(L,n) ? luaL_strlen((L), (n)) : 0)
+
+#define PAD(L, n) (double) (lua_isnumber(L,n) ? luaL_checknumber((L), (n)) : 0.0f)
 
 WFC_API int luaopen_libwfc(lua_State* L);
 
