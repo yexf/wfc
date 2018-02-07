ae93bbc6fa50ad8367856b2de8b647d780449af8
yesheng <yexf_fyf@163.com>
2018-02-07 15:34:21 +0800

    @modify change scheme

--- libwfc/cef_client_scheme.cc
+++ libwfc/cef_client_scheme.cc
@@ -29,13 +29,76 @@ std::string DumpMapJson(const std::map<std::string, std::string>& kvmap)
 {
 	std::stringstream ss;
 	std::string str_splite = "";
-	
+	ss << "{";
+	std::map<std::string, std::string>::const_iterator iter = kvmap.begin();
+	while (iter != kvmap.end())
+	{
+		ss << str_splite;
+		ss << "\"" << iter->first << "\"" << ":";
+		ss << "\"" << iter->second << "\"";
+		str_splite = ",";
+		++iter;
+	}
+	ss << "}";
 	return ss.str();
 }
 
-std::string DumpRequestContents(const std::map<std::string, std::string>& kvmap)
+std::string DumpRequestContents(CefRefPtr<CefRequest> request)
 {
-	return "";
+	std::stringstream ss;
+	ss << "URL: " << std::string(request->GetURL());
+	ss << "\nMethod: " << std::string(request->GetMethod());
+	
+	CefRequest::HeaderMap headerMap;
+	request->GetHeaderMap(headerMap);
+	
+	if (headerMap.size() > 0)
+	{
+		ss << "\nHeaders:";
+		CefRequest::HeaderMap::const_iterator it = headerMap.begin();
+		for (; it != headerMap.end(); ++it)
+		{
+			ss << "\n\t" << std::string(it->first) << ": " << std::string(it->second);
+		}
+	}
+	
+	CefRefPtr<CefPostData> postData = request->GetPostData();
+	if (postData.get())
+	{
+		CefPostData::ElementVector elements;
+		postData->GetElements(elements);
+		if (elements.size() > 0)
+		{
+			ss << "\nPost Data:";
+			CefRefPtr<CefPostDataElement> element;
+			CefPostData::ElementVector::const_iterator it = elements.begin();
+			for (; it != elements.end(); ++it)
+			{
+				element = (*it);
+				if (element->GetType() == PDE_TYPE_BYTES) 
+				{
+					ss << "\n\tBytes: ";
+					size_t size = element->GetBytesCount();
+					if (size == 0)
+					{
+						ss << "(empty)";
+					}
+					else
+					{
+						char *bytes = new char[size];
+						element->GetBytes(size, bytes);
+						ss << std::string(bytes, size);
+						delete[] bytes;
+					}
+				}
+				else if (element->GetType() == PDE_TYPE_FILE)
+				{
+					ss << "\n\tFile: " << std::string(element->GetFile());
+				}
+			}
+		}
+	}
+	return ss.str();
 }
 
 class CefLocalApiScheme : public CefResourceHandler
--- libwfc/cef_client_scheme.h
+++ libwfc/cef_client_scheme.h
@@ -11,8 +11,15 @@
 #pragma once
 #include <set>
 #include "include/cef_browser.h"
+#include "include/cef_callback.h"
+#include "include/cef_frame.h"
+#include "include/cef_resource_handler.h"
+#include "include/cef_response.h"
+#include "include/cef_request.h"
+#include "include/cef_scheme.h"
+#include "include/wrapper/cef_helpers.h"
 
-
+#include "luawfc.h"
 void RegisterSchemeHandlers();
 
 void RegisterLuaSchemeHandlers(const char *scheme_name, const char *domain_name);
